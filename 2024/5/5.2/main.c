05243cbe6793aae2432bc293bc826def5dbb16c0
Alex-TH <alex.trejoh@gmail.com>
2024-12-17 00:08:49 +0700

    day 5: second puzzle solution

--- 2024/5/5.2/main.c
+++ 2024/5/5.2/main.c
@@ -44,12 +44,20 @@ void build_rules(FILE *fd, Rule asc_rules[RULES_ARRAY]) {
   }
 }
 
+void swap_values(unsigned char numbers[RULES_ARRAY], int pos_a, int pos_b) {
+  unsigned char value_a = numbers[pos_a];
+
+  numbers[pos_a] = numbers[pos_b];
+  numbers[pos_b] = value_a;
+}
+
 int validate_line(char line[LINE_BUFFER_SIZE], Rule rules[RULES_ARRAY]) {
   unsigned char numbers[RULES_ARRAY] = {0};
-  unsigned char count[RULES_ARRAY] = {0};
+  char indexes[RULES_ARRAY];
   unsigned char len = 0;
   char *token;
 
+  memset(indexes, -1, sizeof(indexes));
   token = strtok(line, ",");
 
   while (token != NULL) {
@@ -59,19 +67,39 @@ int validate_line(char line[LINE_BUFFER_SIZE], Rule rules[RULES_ARRAY]) {
     len++;
   }
 
+  unsigned char swaped = 0;
   for (int i = 0; i < len; i++) {
     Rule *current_pos = &rules[numbers[i]];
-    count[numbers[i]]++;
+    indexes[numbers[i]] = i;
     while (current_pos != NULL) {
-      if (count[current_pos->value] > 0) {
-        return 0;
+      char current_pos_index = indexes[current_pos->value];
+      if (current_pos_index >= 0) {
+        swaped = 1;
+        swap_values(numbers, current_pos_index, i);
+
+        // start from begining
+        i = -1;
+        memset(indexes, -1, sizeof(indexes));
+        break;
       }
       current_pos = current_pos->next;
     }
   }
 
-  return numbers[len / 2];
+  /* memset(indexes, -1, sizeof(indexes)); */
+  /* for (int i = 0; i < len; i++) { */
+  /*   printf("%hhu,", numbers[i]); */
+  /*   Rule *current_pos = &rules[numbers[i]]; */
+  /*   indexes[numbers[i]] = i; */
+  /*   while (current_pos != NULL) { */
+  /*     char current_pos_index = indexes[current_pos->value]; */
+  /*     if (current_pos_index >= 0) printf("WRONG! XX "); */
+  /*     current_pos = current_pos->next; */
+  /*   } */
+  /* } */
+  /* printf("\n"); */
 
+  return swaped ? numbers[len / 2] : 0;
 }
 
 int main() {
@@ -87,9 +115,23 @@ int main() {
 
   build_rules(fd, asc_rules);
 
+  /* for (int i = 0; i < RULES_ARRAY; i++) { */
+  /*   if (asc_rules[i].value == 0) continue; */
+
+  /*   Rule *current_pos = &asc_rules[i]; */
+  /*   printf("%hhu", i); */
+  /*   while (current_pos != NULL) { */
+  /*     printf("->%hhu", current_pos->value); */
+  /*     current_pos = current_pos->next; */
+  /*   } */
+  /*   printf("\n"); */
+  /* } */
+
   unsigned int solution = 0;
+  int lines = 0;
   while (fgets(line, LINE_BUFFER_SIZE, fd) !=NULL) {
     solution += validate_line(line, asc_rules);
+    lines++;
   }
 
   printf("solution: %d\n", solution);
